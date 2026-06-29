d546464ba138cd5b3918672565400ad5914ee47e
Cosmic <cosmicz211407@gmail.com>
2026-06-30 03:24:57 +0530

    model configuration
    COnfiguration of the model

---
+++ ml_pipeline/config/settings.py
@@ -0,0 +1,12 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv("configuration.env")
+
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+GEMINI_MODEL = os.getenv("GEMINI_MODEL")
+
+GEMINI_TEMPERATURE = float(os.getenv("GEMINI_TEMPERATURE", 0.1))
+GEMINI_TOP_P = float(os.getenv("GEMINI_TOP_P", 0.9))
+GEMINI_TOP_K = int(os.getenv("GEMINI_TOP_K", 40))
+GEMINI_MAX_OUTPUT_TOKENS = int(os.getenv("GEMINI_MAX_OUTPUT_TOKENS", 512))
