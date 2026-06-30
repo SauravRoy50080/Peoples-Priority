b9c418c34e1e4c452f3779bf0a38fbfb2f2ab435
Asmita Jaiswal <asmita3jaiswal@gmail.com>
2026-06-30 12:30:39 +0530

    modified App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,16 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
 import ClusterDetail from './pages/ClusterDetail';
+import Submit from './pages/Submit';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Dashboard />} />
+        <Route path="/" element={<Submit />} />
+        <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/cluster/:clusterId" element={<ClusterDetail />} />
+        <Route path="/submit" element={<Submit />} />
       </Routes>
     </BrowserRouter>
   );
